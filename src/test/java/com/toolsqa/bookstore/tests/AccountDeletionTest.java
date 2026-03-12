161309cec2ec8277d37cb2ea1dba849c1c8ec9e1
Julio Santos <julio958214@gmail.com>
2026-03-12 11:09:49 -0300

    adicionando novas validações

--- src/test/java/com/toolsqa/bookstore/tests/AccountDeletionTest.java
+++ src/test/java/com/toolsqa/bookstore/tests/AccountDeletionTest.java
@@ -6,12 +6,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.google.common.base.Predicates.equalTo;
 import static com.toolsqa.bookstore.utils.Helpers.extractUserId;
 import static com.toolsqa.bookstore.utils.Helpers.generateToken;
 import static org.apache.http.HttpStatus.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 @Tag("deletar_conta")
 @Tag("regression")
@@ -21,25 +20,15 @@ public class AccountDeletionTest extends BaseTest {
     @Tag("criar_e_deletar_conta")
     @DisplayName("Criar uma conta e deletar a mesma")
     void shouldCreateAndDeleteUser() {
-
-        UserModel user = new UserModel(
-                "user" + faker.number().randomNumber(),
-                "Test@123"
-        );
-
-        // cria usuário
+        UserModel user = new UserModel("user" + faker.number().randomNumber(), "Test@123");
         response = createUser(user);
 
-        response.then()
-                .statusCode(SC_CREATED);
-
         String userId = extractUserId(response);
-
         String token = generateToken(user);
 
-        // deleta usuário
-        deleteUser(userId, token)
-                .then()
+        response = deleteUser(userId, token);
+        response.then()
+                .body(equalTo(""))
                 .statusCode(SC_NO_CONTENT);
     }
 
@@ -47,16 +36,14 @@ public class AccountDeletionTest extends BaseTest {
     @Tag("deletar_com_id_invalido")
     @DisplayName("Deletar uma conta com ID inválido")
     void shouldNotDeleteUserWithInvalidId() {
-        UserModel user = new UserModel(
-                "user" + faker.number().randomNumber(),
-                "Test@123"
-        );
-
+        UserModel user = new UserModel("user" + faker.number().randomNumber(), "Test@123");
         String token = generateToken(user);
 
         deleteUser("000000000", token)
                 .then()
                 .statusCode(SC_OK)
+                .body("code", equalTo("1207"))
+                .body("message", equalTo("User Id not correct!"))
                 .body(matchesJsonSchemaInClasspath("resources/contracts/deleteUser/delete-user-invalid-id.json"));
     }
 
@@ -65,23 +52,18 @@ public class AccountDeletionTest extends BaseTest {
     @Tag("deletar_e_falhar_login")
     @DisplayName("Deletar uma conta e tentar realizar login com a mesma")
     void shouldNotLoginAfterDeletingUser() {
-
-        UserModel user = new UserModel(
-                "user" + faker.number().randomNumber(),
-                "Test@123"
-        );
-
+        UserModel user = new UserModel("user" + faker.number().randomNumber(), "Test@123");
         response = createUser(user);
 
         String userId = extractUserId(response);
         String token = generateToken(user);
-
-        deleteUser(userId, token)
-                .then()
-                .statusCode(SC_NO_CONTENT);
+        deleteUser(userId, token);
 
         response = loginUser(user);
         response.then()
-                .statusCode(SC_OK);
+                .body("token", equalTo(null))
+                .body("expires", equalTo(null))
+                .body("status", equalTo("Failed"))
+                .body("result", equalTo("User authorization failed."));
     }
 }
