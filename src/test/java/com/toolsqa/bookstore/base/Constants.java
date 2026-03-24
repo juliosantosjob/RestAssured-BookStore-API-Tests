974aecf35b7494bded1e917bf6daa63c55853fdb
juliosantosjob <juliosantosjob@users.noreply.github.com>
2026-03-24 20:40:57 +0000

    Automated deployment: Tue Mar 24 20:40:56 UTC 2026 c6e27f5a6949c94ed80e2378fd7e6980bde1f9e4

--- src/test/java/com/toolsqa/bookstore/base/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.toolsqa.bookstore.base;
-
-public interface Constants {
-    String CONTENT_TYPE = "application/json";
-    Long MAX_TIMEOUT = 5000L;
-}
--- src/test/java/com/toolsqa/bookstore/base/Routes.java
+++
@@ -1,9 +0,0 @@
-package com.toolsqa.bookstore.base;
-
-public interface Routes {
-    String CREATE_USER = "/Account/v1/User";
-    String LOGIN = "/Account/v1/GenerateToken";
-    String GET_USER = "/Account/v1/User/";
-    String BOOKS = "/BookStore/v1/Books";
-    String DELETE_USER = "/Account/v1/User/";
-}
